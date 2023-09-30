388e603a50663fee0bc108164fffd3feb77bd106
Oleksandr <bajmutov@gmail.com>
2023-09-30 14:14:36 +0300

    spiner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,14 @@
-import { Circles } from 'react-loader-spinner';
+import { Blocks } from 'react-loader-spinner';
 
 export const Loader = () => {
   return (
-    <Circles
-      height="100"
-      width="100"
-      color="#4fa94d"
-      ariaLabel="circles-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
+    <Blocks
       visible={true}
+      height="80"
+      width="80"
+      ariaLabel="blocks-loading"
+      wrapperStyle={{}}
+      wrapperClass="blocks-wrapper"
     />
   );
 };
