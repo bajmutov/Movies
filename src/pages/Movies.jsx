4bf99e4217900e51b5c040f0d95df97700559bdb
Oleksandr <bajmutov@gmail.com>
2023-09-30 13:46:20 +0300

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,16 @@
 import { fetchMovieByName } from 'Api/getApi';
+import { Loader } from 'components/Loader/Loader';
 import SearchBox from 'components/SearchBox/SearchBox';
 import Notiflix from 'notiflix';
 import { useEffect, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const movieName = searchParams.get('movieName') ?? '';
   const [movies, setMovies] = useState([]);
+  const location = useLocation();
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -25,11 +28,13 @@ const Movies = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
+        setIsLoading(true);
         const movies = await fetchMovieByName(movieName);
-        console.log('movies', movies);
         setMovies(movies);
       } catch (error) {
         Notiflix.Notify.failure(error.message);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchMovies();
@@ -46,10 +51,13 @@ const Movies = () => {
   return (
     <>
       <SearchBox onSubmit={handleSubmit} />
+      {isLoading && <Loader />}
       <ul>
         {movies.map(({ id, title }) => (
           <li key={id}>
-            <Link to={`/movies/${id}`}>{title}</Link>
+            <Link to={`/movies/${id}`} state={{ from: location }}>
+              {title}
+            </Link>
           </li>
         ))}
       </ul>
